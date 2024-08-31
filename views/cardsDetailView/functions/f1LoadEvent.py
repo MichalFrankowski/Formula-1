e23ea566444b37e642d0d5a001b8a0472ec9a45b
MichalFrankowski <119890552+MichalFrankowski@users.noreply.github.com>
2024-08-31 21:00:03 +0100

    moving function that loades event to seperate file ind function folder

---
+++ views/cardsDetailView/functions/f1LoadEvent.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import fastf1
+
+# Cache the data fetching process to prevent re-fetching on every interaction
+@st.cache_data
+def load_event(season_year):
+    schedule = fastf1.get_event_schedule(int(season_year))
+    schedule['EventDate'] = pd.to_datetime(schedule['EventDate']).dt.strftime('%d %b %Y')
+    return schedule
+
+if __name__ == "__main__":
+    load_event()
